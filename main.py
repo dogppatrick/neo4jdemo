1fe0add74dd5c8f0fda63229efaf0e71d2824d26
patrick <patrick@stockfeel.com.tw>
2022-04-20 17:50:05 +0800

    add run query sample

--- main.py
+++ main.py
@@ -3,14 +3,24 @@ from adaptor.databases import Neo4j
 n4j = Neo4j()
 geo = n4j.g
 
-query = '''CALL gds.graph.create(
-    'Membersimilary2',
-    ['Post', 'Member'],
-    {
-        Read: {
-            type: 'Read'
-        }
-    }
-);
+# query = '''CALL gds.graph.create(
+#     'Membersimilary2',
+#     ['Post', 'Member'],
+#     {
+#         Read: {
+#             type: 'Read'
+#         }
+#     }
+# );
+# '''
+# geo.run(query)
+
+query = '''
+CALL gds.nodeSimilarity.stream('Membersimilary2')
+YIELD node1, node2, similarity
+Where similarity > 0.5
+RETURN gds.util.asNode(node1).id AS Member1, gds.util.asNode(node2).id AS Member2, similarity
+ORDER BY similarity DESCENDING, Member1, Member2
 '''
-geo.run(query)
+result = geo.run(query).to_data_frame()
+result.to_csv('Membersimilary2.csv',index=False)
